d8935e542de1a68d7e4a336d8afba0c31f0e62fd
Ian MacDonald <IanMacDonald@nanometrics.ca>
2024-07-04 13:23:21 -0400

    added more exports so .d.ts files link.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,19 @@
 export { default as SchemaForm } from "./SchemaForm.svelte";
 export { default as SubmitForm } from "./SubmitForm.svelte";
+
+export { default as Array } from "./editors/Array.svelte";
+export { default as ArrayBlocks } from "./editors/ArrayBlocks.svelte";
+export { default as Autocomplete } from "./editors/Autocomplete.svelte";
+export { default as Boolean } from "./editors/Boolean.svelte";
+export { default as Enum } from "./editors/Enum.svelte";
+export { default as FieldWrapper } from "./editors/FieldWrapper.svelte";
+export { default as Hidden } from "./editors/Hidden.svelte";
+export { default as ListDetail } from "./editors/ListDetail.svelte";
+export { default as Number } from "./editors/Number.svelte";
+export { default as Object } from "./editors/Object.svelte";
+export { default as Radio } from "./editors/Radio.svelte";
+export { default as String } from "./editors/String.svelte";
+export { default as StringDateTime } from "./editors/TextArea.svelte";
+export { default as Upload } from "./editors/Upload.svelte";
+
+export type { CommonComponentParameters } from "./types/CommonComponentParameters";
